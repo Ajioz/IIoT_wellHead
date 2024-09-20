20969932d77ab3bf488657eee98a2ef0b629a9c9
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-20 13:19:47 +0100

    bug dind

--- main/main.ino
+++ main/main.ino
@@ -3,19 +3,19 @@
 
 
 /*Put your SSID & Password*/
-const char* WIFI_SSID = "AjiozDev";  // Enter SSID here
-const char* WIFI_PASS = "sandiewhyte123";  //Enter Password herez
+const char* WIFI_SSID = "xxxxxxx";  // Enter SSID here
+const char* WIFI_PASS = "xxxxxxx";  //Enter Password herez
 
 // Losant Smart Pump-Control credentials
-//const char* LOSANT_DEVICE_ID = "63f4bd0487b0e03668133368";
-//const char* LOSANT_ACCESS_KEY = "0f05bc18-14a5-441d-8052-9844d06247be";
-//const char* LOSANT_ACCESS_SECRET = "897e12416f5abe5233b8b617bb77e81c054c19197860d2f5a9660e5d2abaf570";
+//const char* LOSANT_DEVICE_ID = "xxxxxxx";
+//const char* LOSANT_ACCESS_KEY = "xxxxxxx";
+//const char* LOSANT_ACCESS_SECRET = "xxxxxxx";
 
 
 // Losant eHealth credentials
-const char* LOSANT_DEVICE_ID = "624020db4924f62857bc4695";
-const char* LOSANT_ACCESS_KEY = "2fe39268-bea5-4141-9b73-11b7d80fce30";
-const char* LOSANT_ACCESS_SECRET = "f6c21976af3c0d2de00a294b9a3cc1183fe5ececca1cfd6f08df2f2ca06d3310";
+const char* LOSANT_DEVICE_ID = "xxxxxxx";
+const char* LOSANT_ACCESS_KEY = "xxxxxxx";
+const char* LOSANT_ACCESS_SECRET = "xxxxxxx";
 
 
 //Defining the constant
@@ -51,26 +51,7 @@ uint8_t timeSinceLastReadgas = 0;
 
 const char* rootCABuff = \
 "-----BEGIN CERTIFICATE-----\n" \
-"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
-"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
-"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
-"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
-"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
-"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
-"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
-"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
-"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
-"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
-"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
-"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
-"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
-"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
-"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
-"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
-"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
-"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
-"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
-"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+"xxxxxxx" \
 "-----END CERTIFICATE-----\n";
 
 WiFiClientSecure wifiClient;
